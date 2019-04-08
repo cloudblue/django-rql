e388f39426ceb440b0aa1b60b8b73ca6ee5ae9ae
Max Kolyubyakin <Maxim.Kolyubyakin@ingrammicro.com>
2019-04-08 17:52:37 +0300

    Removed unneeded code and better flake configuration.

--- dj_rql/parser.py
+++ dj_rql/parser.py
@@ -7,9 +7,3 @@ from dj_rql.grammar import RQL_GRAMMAR
 
 
 RQLParser = Lark(RQL_GRAMMAR, parser='lalr', start='start')
-
-
-if __name__ == '__main__':
-    r = RQLParser.parse('and(and=or,or=and)')
-    print(print(r.pretty()))
-    pass
