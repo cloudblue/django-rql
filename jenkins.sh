60bb2d41567f5f745cf8ba13810231b2e2b5a132
Max Kolyubyakin <Maxim.Kolyubyakin@ingrammicro.com>
2019-08-01 16:02:57 +0300

    Sonar is turned of for now.

--- jenkins.sh
+++ jenkins.sh
@@ -19,16 +19,16 @@ export PATH=$PATH:/opt/sonar-scanner-2.6.1/bin/
 export VERSION=$(cat VERSION)
 export PR_ID=`git branch -a --contains ${GIT_COMMIT} | grep 'remotes/origin/pr/[0-9]*/' | head -1 | sed 's/[^0-9]*//g'`
 
-sonar-scanner \
-    -Dsonar.projectVersion=$VERSION \
-    -Dsonar.stash.project=SWFT \
-    -Dsonar.stash.repository=django-rql \
-    -Dsonar.stash.pullrequest.id=$PR_ID \
-    -Dsonar.stash.notification=true \
-    -Dsonar.stash.comments.reset=false \
-    -Dsonar.stash.login=commit-blocker-bot \
-    -Dsonar.stash.report.issues=true \
-    -Dsonar.stash.report.line=false \
-    -Dsonar.stash.report.coverage=true
+#sonar-scanner \
+#    -Dsonar.projectVersion=$VERSION \
+#    -Dsonar.stash.project=SWFT \
+#    -Dsonar.stash.repository=django-rql \
+#    -Dsonar.stash.pullrequest.id=$PR_ID \
+#    -Dsonar.stash.notification=true \
+#    -Dsonar.stash.comments.reset=false \
+#    -Dsonar.stash.login=commit-blocker-bot \
+#    -Dsonar.stash.report.issues=true \
+#    -Dsonar.stash.report.line=false \
+#    -Dsonar.stash.report.coverage=true
 
 rm -rf django_rql_env
