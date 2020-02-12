4c3346ad6405f5834462814c3a41811ae1bb0d29
maximus <maxim.kolyubyakin@ingrammicro.com>
2020-02-12 17:46:15 +0300

    Prepared potentially full list of serializers for RND of select().

--- tests/dj_rf/view.py
+++ tests/dj_rf/view.py
@@ -11,7 +11,65 @@ from dj_rql.drf import (
 )
 from dj_rql.compat import DjangoFiltersRQLFilterBackend
 from tests.dj_rf.filters import BooksFilterClass
-from tests.dj_rf.models import Book
+from tests.dj_rf.models import Author, Book, Page, Publisher
+
+
+class PublisherReferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = ('id', 'name')
+
+
+class AuthorReferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ('id', 'name')
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    publisher = PublisherReferenceSerializer()
+
+    class Meta:
+        model = Author
+        fields = (
+            'id',
+            'name',
+            'publisher',
+        )
+
+
+class PageReferenceSerializer(serializers.ModelSerializer):
+    id = serializers.CharField(source='uuid')
+
+    class Meta:
+        model = Page
+        fields = (
+            'id',
+            'content',
+        )
+
+
+class SelectBookSerializer(serializers.ModelSerializer):
+    write_only = serializers.CharField(source='title', write_only=True)
+
+    author_ref = AuthorReferenceSerializer(source='author')
+    author = serializers.SerializerMethodField()
+    pages = PageReferenceSerializer(many=True)
+
+    class Meta:
+        model = Book
+        fields = (
+            'id',  # must always be shown in both detail and list
+            'blog_rating',  # by default not shown in list, but shown in detail
+            'github_stars',  # by default not shown anywhere
+            'write_only',
+            'author_ref',  # One level reference field (FK)
+            'author',  # Deep nested fields (FK)
+            'pages',  # List of backrefs
+        )
+
+    def get_author(self, obj):
+        return AuthorSerializer(obj).data
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -43,3 +101,7 @@ class DRFViewSet(BaseViewSet):
 
 class DjangoFiltersViewSet(BaseViewSet):
     filter_backends = (DjangoFiltersRQLFilterBackend,)
+
+
+class SelectViewSet(mixins.RetrieveModelMixin, BaseViewSet):
+    serializer_class =
